fbb3611295014fde906cf6e561784ad547b3f6f7
sophialou <soph@u.northwestern.edu>
2016-09-27 20:54:10 -0500

    almost working

--- xv6/user/ps.c
+++ xv6/user/ps.c
@@ -6,10 +6,15 @@
 void
 ps(void)
 {
-	struct ProcessInfo table;
-	int numProcessesGotten = getprocs(&table);
-	//int numProcessesGotten = getprocs(600);
-	printf(1,"number of processes %d", numProcessesGotten);
+	struct ProcessInfo *table = NULL;
+	char *stateMapping[] = {"UNUSED","EMBRYO","SLEEP","RUNNABLE","RUNNING","ZOMBIE"};
+	int numProcessesGotten = getprocs(table);
+	printf(1,"number of processes %d \n", numProcessesGotten);
+
+	printf(1,"%d %d %s %d %s \n", table->pid, table->ppid, stateMapping[table->state], table->sz, table->name);
+	//printf(1,"%d %d %s %d %s \n", table[0].pid, table[0].ppid, stateMapping[table[0].state], table[0].sz, table[0].name);
+
+
 }
 
 int 
